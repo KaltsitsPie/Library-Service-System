c36426682321a68a1645865dd0e87ba429b2e676
zyc <beinu2137@gmail.com>
2020-12-20 00:42:18 +0800

    init: LentBook

---
+++ LentBook.java
@@ -0,0 +1,23 @@
+public class LentBook extends Book {
+    //归还期限，用yyyy年MM月dd日表示
+    private String date;
+    //借阅者姓名
+    private String readerName;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
+
+    public String getReaderName() {
+        return readerName;
+    }
+
+    public void setReaderName(String readerName) {
+        this.readerName = readerName;
+    }
+}
